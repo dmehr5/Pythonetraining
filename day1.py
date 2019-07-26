b12095ef4a37c8332781927af3e80cffa87e37bc
dmehr5 <53352264+dmehr5@users.noreply.github.com>
2019-07-27 01:03:30 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+name="Tony Stark"
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+
+# In[3]:
+
+
+first_name="Tony"
+last_name="stark"
+full_name=f"{first_name} {last_name}"
+print(full_name)
+print(full_name.title())
+
+
+# In[5]:
+
+
+print("python")
+print("python ")
+#no difference in above statement even though whitespace is there in second statement
+print('\tpython')
+#for tab space use \t
+
+
+# In[8]:
+
+
+declare="somestring"
+declare1='somestring1'
+declare2="""somestring2"""
+#no difference in above three statement except that to include the afstope in between the string we need double quotes
+example="john's dad name is samuel"
+
+
+# In[11]:
+
+
+#To print new line
+print("Language:python\n\tc\n\tjava")
+
+
+# In[16]:
+
+
+#Stripping the whitespaces
+fav_lang="   python   "
+print(fav_lang)
+print(fav_lang.rstrip())
+print(fav_lang.lstrip())
+print(fav_lang.strip())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
